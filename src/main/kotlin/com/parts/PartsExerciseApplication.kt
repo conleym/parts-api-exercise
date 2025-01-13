d6808330d9e26eaa00b3d54e09bad3f8e9013c01
Mike Conley <conleym@users.noreply.github.com>
2025-01-13 12:59:04 -0500

    Initial commit.

---
+++ src/main/kotlin/com/parts/PartsExerciseApplication.kt
@@ -0,0 +1,11 @@
+package com.parts
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PartsExerciseApplication
+
+fun main(args: Array<String>) {
+    runApplication<PartsExerciseApplication>(*args)
+}
---
+++ src/test/kotlin/com/parts/PartsExerciseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.parts
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PartsExerciseApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
